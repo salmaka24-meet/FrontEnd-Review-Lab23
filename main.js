8ebc6c77ff0157e5e53369508ae55eb77b819b36
salma.lalalands <salmak6010@gmail.com>
2023-07-17 18:08:43 +0300

    lab done

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 function changeBackgroundColor(color) 
-{document.getElementById().style.changeBackgroundColor = lightblue;}
+{
+  document.body.style.backgroundColor =color;
+}
+
+function changeBackgroundColor2(color1, color2) 
+{
+	var currentColor = document.body.style.backgroundColor;
+	
+	if (currentColor == color1){
+		changeBackgroundColor(color2);
+	}
+	else {
+		changeBackgroundColor(color1);
+	}
+}
+
+
